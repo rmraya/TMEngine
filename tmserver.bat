609a6a82cd0b947f5d6c0f91006fb5343ceb4c2c
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-11-01 15:08:33 -0300

    Fixed launcher

--- tmserver.bat
+++ tmserver.bat
@@ -3,4 +3,4 @@ pushd "%~dp0"
 
 set CP="lib\mariadb-java-client-2.4.3.jar"
 
-bin/java -cp %CP% --module-path lib com.maxprograms.tmserver.TmServer $@
+.\bin\java -cp %CP% --module-path lib com.maxprograms.tmserver.TmServer $@
